ad27f440f3fe183ffb73aa4239f9b8d941028458
Arthur Guigand <aguigand@umanit.fr>
2016-12-23 17:57:26 +0100

    :christmas_tree: :christmas_tree: :christmas_tree: :christmas_tree:

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 require 'vendor/autoload.php';
 
 $smtpUsername = 'your_email@gmail.com';
-$smtppassword = 'your_password';
+$smtpPassword = 'your_password';
 
 $team = [
-    "email@umanit.fr" => "name",
+    "email1@yo.yo" => "name1",
+    "email2@yo.yo" => "name2",
 ];
 
 
@@ -24,14 +25,41 @@ foreach ($team as $key => $val) {
     unset($team[$key]);
 }
 
-$teamPool = $team;
-
-shuffle($teamPool);
+$teamPool = getShuffledTeam($team);
 
 foreach ($team as $key => $employee) {
-    sendSecretSantaEmail($employee, array_pop($teamPool), $mailer, $smtpUsername);
+    sendSecretSantaEmail($employee, $teamPool[$key], $mailer, $smtpUsername);
 }
 
+/**
+ * Shuffle santas ensuring santa's santa is not santa.
+ *
+ * @param array $team
+ *
+ * @return mixed
+ */
+function getShuffledTeam($team)
+{
+    $teamPool = $team;
+    shuffle($teamPool);
+
+    foreach ($team as $key => $item) {
+        if ($teamPool[$key]['email'] === $item['email']) {
+            return getShuffledTeam($team);
+        }
+    }
+
+    return $teamPool;
+}
+
+/**
+ * Send email
+ *
+ * @param  array       $santa
+ * @param  array       $target
+ * @param Swift_Mailer $mailer
+ * @param  string      $smtpUsername sender's email
+ */
 function sendSecretSantaEmail($santa, $target, Swift_Mailer $mailer, $smtpUsername)
 {
     // Create the message
